6e399e26c0cec882f290f10f8ce1f34043026021
Antonin Durand <antonin.durand576@gmail.com>
2025-10-14 14:01:53 +0200

    Init application structure

---
+++ goatTutorats/src/main/java/com/example/goatTutorats/GoatTutoratsApplication.java
@@ -0,0 +1,13 @@
+package com.example.goatTutorats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatTutoratsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatTutoratsApplication.class, args);
+	}
+
+}
---
+++ goatTutorats/src/test/java/com/example/goatTutorats/GoatTutoratsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goatTutorats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatTutoratsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
