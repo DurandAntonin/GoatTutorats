ea2df2918cfadcb1b50f22777145eb26ecf4d18c
Etsu7 <mateo.massinond@gmail.com>
2025-10-17 16:00:38 +0200

    Remove unused import

--- goatTutorats/src/main/java/com/example/goatTutorats/controlers/ApprenticeController.java
+++ goatTutorats/src/main/java/com/example/goatTutorats/controlers/ApprenticeController.java
@@ -2,7 +2,6 @@ package com.example.goatTutorats.controlers;
 
 import com.example.goatTutorats.dtos.ApprenticeRecordDTO;
 import com.example.goatTutorats.services.ApprenticeService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
